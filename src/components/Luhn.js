c4ff20f4ae722598bea93abd9f812a0045075af0
Jlyall23 <jordanlyall23@gmail.com>
2023-04-13 17:40:18 -0400

    working!

--- src/components/Luhn.js
+++ src/components/Luhn.js
@@ -1,51 +1,48 @@
-import React,{useState} from "react"
-let validMessage= document.getElementById("message")
+import React, { useState, useEffect } from "react";
+function Luhn() {
+  const [cardNumber, setCardNumber] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  useEffect(() => {
 
-function Luhn(){
-  const[cardNumber,setCardNumber]=useState("")
-  let[isValid,setIsValid]=useState(false)
-  
-
-function validateCardNumber(){
-    
-  let sum = 0
-  let isEven = false
-
-for(let i=cardNumber.length -1;i>=0;i--){
-    let digit =parseInt(cardNumber.charAt(i))
-    if(isEven){
-        digit *=2
-    if(digit >9){
-        digit -=9
+    validateCardNumber();
+  }, [cardNumber]);
+  function validateCardNumber() {
+    let sum = 0;
+    let isEven = false;
+    for (let i = cardNumber.length - 1; i >= 0; i--) {
+      let digit = parseInt(cardNumber.charAt(i), 10);
+      if (isEven) {
+        digit *= 2;
+        if (digit > 9) {
+          digit -= 9;
+        }
+      }
+      sum += digit;
+      isEven = !isEven;
     }
-    }
-    sum+=digit
-    isEven=!isEven
-    console.log(sum)
-    
-}
-    if(sum %10 === 0){
-        isValid=true
-        console.log(isValid)
-        validMessage.textContent='Card Number is VALID!'
-
-    }else{
-        isValid=false
-    }console.log(isValid)
-    validMessage.textContent='Card Number is INVALID!'
-
-    
-}
-return(
-<div>
-    <input type ='text' value = {cardNumber} onChange={(e)=>
-    setCardNumber(e.target.value)}/>
-   
-    <button onClick={validateCardNumber}>Validate</button>
-
-    
-</div>
-)
-
+    setIsValid(sum % 10 === 0);
+  }
+  function handleInputChange(event) {
+    setCardNumber(event.target.value);
+  }
+  function handleValidationClick() {
+    validateCardNumber();
+  }
+  return (
+    <div>
+      <label>
+        Card Number:
+        <input type="text" value={cardNumber} onChange={handleInputChange} />
+      </label>
+      <br />
+      <button onClick={handleValidationClick}>Validate</button>
+      <br />
+      {isValid ? (
+        <p style={{ color: "green" }}>Valid card number!</p>
+      ) : (
+        <p style={{ color: "red" }}>Invalid card number.</p>
+      )}
+    </div>
+  );
 }
 export default Luhn;
